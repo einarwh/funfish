ac5723e1c2a111627a17425d0deea748e81fd2c5
Einar W. Høst <einarwh@gmail.com>
2017-02-17 17:00:53 +0100

    updated script

--- Funfish/start.fsx
+++ Funfish/start.fsx
@@ -1,4 +1,5 @@
 #I @"../packages/"
 #r @"NGraphics/lib/net45/NGraphics.dll"
 
-#load "Points.fs" "Vectors.fs" "Segments.fs" "Curves.fs" "Rectangles.fs" "Pictures.fs" "Mapping.fs" "Drawing.fs" "Bitmaps.fs" "Svg.fs" "Program.fs"
+#load "Points.fs" "Vectors.fs" "Segments.fs" "Curves.fs" "Data.fs" "Rectangles.fs" "Pictures.fs" "Mapping.fs" "Drawing.fs" "Letters.fs" "SquareLimit.fs" "Bitmaps.fs" "Svg.fs" "Render.fs" "Program.fs"
+
