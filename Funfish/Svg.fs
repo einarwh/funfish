f02fa87a1b0c2ff19b8a07d3e5e90610b11a1cbd
Einar Høst <Einar.Host@nrk.no>
2017-07-21 23:53:26 +0200

    dead code

--- Funfish/Svg.fs
+++ Funfish/Svg.fs
@@ -15,9 +15,6 @@ open Styling
 let mapper { a = a; b = b; c = c } = function  
   | { x = x; y = y } -> a + b * x + c * y
 
-let mirror ymax { x = x; y = y } =
-  { x = x; y = ymax - y}
-
 let mapShape m = function 
   | Polygon { points = pts } ->
     Polygon { points = pts |> List.map m } 
@@ -85,7 +82,6 @@ let getColor name = function
     else if name = "eye-inner" then StyleColor.Black 
     else StyleColor.Black
 
-
 let getEyeLiner sw hue =  
   { strokeColor = getColor "secondary" hue 
     strokeWidth = sw / 1.5 }
@@ -113,18 +109,6 @@ let mapNamedShape (box : Box, hue : Hue) (name, shape) : (Shape * Style) =
     let style = getPathStyle name sw hue
     Path (m start, beziers |> List.map (mapBezier m)), style
                      
-//let shapeVectors = function 
-//  | Polygon { points = pts } -> pts
-
-//let getVectors (shapes : Shape list) = 
-//  shapes |> List.collect shapeVectors
-
-//let bound (shapes : Shape list) = 
-//  let vectors = shapes |> List.collect shapeVectors
-//  let xmax = vectors |> List.map (fun { x = x; y = _ } -> x) |> List.max
-//  let ymax = vectors |> List.map (fun { x = _; y = y } -> y) |> List.max
-//  (xmax, ymax)
-
 let getStyle box = 
   let sw = getStrokeWidth box
   { stroke = Some { strokeWidth = sw
@@ -158,12 +142,11 @@ let getFillBrush { fillColor = fc } =
 let renderSvg (width : float) (height : float) (filename : string) (styledShapes : (Shape * Style) list) = 
   let size = Size(width, height)
   let canvas = GraphicCanvas(size)
-  let color, defaultPen = Colors.Black, Pens.Black
-  let p x y = Point(x, height - y) // Point(x, ymax - y)?
+  let p x y = Point(x, height - y) 
   let getPen style = 
     match style.stroke with 
     | Some stroke -> getStrokePen stroke 
-    | None -> defaultPen
+    | None -> Pens.Black
   let drawShape (style : Style) = function 
   | Polygon { points = pts } ->
     match pts |> List.map (fun { x = x; y = y } -> p x y) with 
@@ -203,62 +186,3 @@ let renderSvg (width : float) (height : float) (filename : string) (styledShapes
   use writer = new StreamWriter(filename)
   canvas.Graphic.WriteSvg(writer)
 
-//let renderSvg' (width : float) (height : float) (filename : string) (styledShapes : (string * Shape * Style) list) = 
-//  let size = Size(width, height)
-//  let canvas = GraphicCanvas(size)
-//  let color, defaultPen = Colors.Black, Pens.Black
-//  let p x y = Point(x, height - y) // Point(x, ymax - y)?
-//  let getPen style = match style.stroke with Some stroke -> defaultPen.WithWidth(stroke.strokeWidth) | None -> defaultPen
-//  let drawShape (name : string) (style : Style) = function 
-//  | Polygon { points = pts } ->
-//    match pts |> List.map (fun { x = x; y = y } -> p x y) with 
-//    | startPoint :: t ->
-//      let move = MoveTo(startPoint) :> PathOp
-//      let lines = t |> List.map (fun pt -> LineTo(pt) :> PathOp) 
-//      let close = ClosePath() :> PathOp
-//      let ops = (move :: lines) @ [ close ] 
-//      canvas.DrawPath(ops, getPen style)
-//    | _ -> ()
-//  | Curve { point1 = { x = x1; y = y1 }
-//            point2 = { x = x2; y = y2 }
-//            point3 = { x = x3; y = y3 } 
-//            point4 = { x = x4; y = y4 } } ->
-//    let pt1, pt2, pt3, pt4 = p x1 y1, p x2 y2, p x3 y3, p x4 y4
-//    let move  = new MoveTo(pt1) :> PathOp
-//    let curve = new CurveTo(pt2, pt3, pt4) :> PathOp
-//    canvas.DrawPath([move; curve], getPen style)  
-//  styledShapes |> List.iter (fun (name, shape, style) -> drawShape name style shape)
-//  use writer = new StreamWriter(filename)
-//  canvas.Graphic.WriteSvg(writer)
-
-
-(**
-let createSvgPicture (canvas : GraphicCanvas) (shapes : Shape list) : Picture = 
-  fun box -> 
-    let height = canvas.Graphic.Size.Height
-    let color = Colors.Black
-    let p x y = Point(x, height - y)
-    let pen = Pens.Black.WithWidth(0.5);
-    let drawShape : Shape -> unit = function 
-    | Polygon { points = pts } ->
-      match pts |> List.map (fun { x = x; y = y } -> p x y) with 
-      | startPoint :: t ->
-        let move = MoveTo(startPoint) :> PathOp
-        let lines = t |> List.map (fun pt -> LineTo(pt) :> PathOp) 
-        let close = ClosePath() :> PathOp
-        let ops = (move :: lines) @ [ close ] 
-        canvas.DrawPath(ops, pen)
-      | _ -> ()
-    | Curve { point1 = { x = x1; y = y1 }
-              point2 = { x = x2; y = y2 }
-              point3 = { x = x3; y = y3 } 
-              point4 = { x = x4; y = y4 } } ->
-      let pt1, pt2, pt3, pt4 = p x1 y1, p x2 y2, p x3 y3, p x4 y4
-      let move  = new MoveTo(pt1) :> PathOp
-      let curve = new CurveTo(pt2, pt3, pt4) :> PathOp
-      canvas.DrawPath([move; curve], pen)  
-    shapes |> List.iter drawShape
-
-**)
-
-  
